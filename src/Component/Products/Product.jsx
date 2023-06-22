61acc4c842f582f75cb033a6e985978b90b4459f
Saidul-06 <saidul15-2703@diu.edu.bd>
2023-06-22 20:06:24 +0600

    reTry

---
+++ src/Component/Products/Product.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Product.css";
+const Product = (Props) => {
+  console.log(Props);
+  const { name, price, seller, ratings, img } = Props.Allproducts;
+
+ 
+  return (
+    <div className="Prdcuts">
+      <img src={img} alt="" />
+
+      <div className="Product_info">
+        <h6 className="Product_name"> Name : {name}</h6>
+        <h3>Price : {price}</h3>
+        <h3>Manufecturer : {seller}</h3>
+        <h3>Rating : {ratings}</h3>
+      </div>
+
+      <button onClick={() => handleAddToCart(Props)} className="button-cart">Add to Cart</button>
+    </div>
+  );
+};
+
+export default Product;
